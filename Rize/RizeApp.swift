2a286cf1b9d6b2d0c5395607bdb498fa6bb34938
stepdugas <131499172+stepdugas@users.noreply.github.com>
2026-03-07 12:33:26 -0500

    Initial Commit

---
+++ Rize/RizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RizeApp.swift
+//  Rize
+//
+//  Created by Stephanie Dugas on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct RizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
